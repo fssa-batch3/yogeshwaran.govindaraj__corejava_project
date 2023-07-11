5ed08d43958e8b6f3a656419c2642d2ddf549a20
yogesh-maari-1602 <118976696+yogesh-maari-1602@users.noreply.github.com>
2023-07-11 16:51:47 +0530

    day 02

---
+++ src/main/java/day01/solved/packageTwo/AccessBicycle.java
@@ -0,0 +1,13 @@
+
+package day01.solved.packageTwo;
+import day01.solved.packageOne.Bicycle;
+
+
+public class AccessBicycle {
+	public static void main(String[] args) {
+		Bicycle bike1 = new Bicycle();
+//		System.out.println(bike1.gear);
+		bike1.getGear();
+		
+	}
+}
