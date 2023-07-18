8e96ca3dbb7bddc7e1946396fde50bde2e1a223c
yogesh-maari-1602 <118976696+yogesh-maari-1602@users.noreply.github.com>
2023-07-18 18:09:35 +0530

    commit day07

---
+++ src/main/java/day07/solved/TreeSetDemo.java
@@ -0,0 +1,21 @@
+package day07.solved;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+ 
+    public static void main(String[] args) {
+ 
+        // TreeSet sorts the collections
+        TreeSet<String> cityNames = new TreeSet<String>();
+        cityNames.add("Delhi");
+        cityNames.add("Chennai");
+        cityNames.add("Bangalore");
+ 
+        for (String cityName : cityNames) {
+            System.out.println(cityName);
+        }
+         
+         
+    }
+}
