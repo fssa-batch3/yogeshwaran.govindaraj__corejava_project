d607cdb47a37f803a81189bd282775aae9335370
yogesh-maari-1602 <118976696+yogesh-maari-1602@users.noreply.github.com>
2023-07-27 14:03:34 +0530

    commit

---
+++ src/main/java/day01/practice/Number.java
@@ -0,0 +1,17 @@
+package day01.practice;
+
+public class Number {
+	
+	int x = 10;
+	int y = 14;
+	
+	public static void main(String[]args) {
+		
+		Number obj1 = new Number();
+		System.out.println(obj1.x);
+	//	Number obj2 = new Number();
+		System.out.println(obj1.y);
+		
+		
+	}
+}
